39be3b133dafb1245b5ec5692113ed9dd9605741
RevathiJambunathan <revanathan@gmail.com>
2020-01-21 14:24:03 -0800

    particles injected below surface, dipole magnetic field inside and outside, momentum of all particles is zero, fixed function that applied vacuum EB fields on particles

--- Source/Particles/MultiParticleContainer.cpp
+++ Source/Particles/MultiParticleContainer.cpp
@@ -1487,4 +1487,13 @@ MultiParticleContainer::PulsarParticleInjection()
         pc->PulsarParticleInjection(); 
     }
 }
+void 
+MultiParticleContainer::PulsarParticleRemoval()
+{
+    amrex::Print() << " pulsar particle removal on! \n";
+    for (auto& pc : allcontainers) {
+        pc->PulsarParticleRemoval(); 
+    }
+
+}
 #endif
