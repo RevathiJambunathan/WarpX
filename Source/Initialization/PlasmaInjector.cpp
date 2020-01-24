c7203ae8b6f169a2cc890b03cb0d4a29cf028b5a
RevathiJambunathan <revanathan@gmail.com>
2020-01-23 17:39:27 -0800

    removing unwanted insidePulsarBound function

--- Source/Initialization/PlasmaInjector.cpp
+++ Source/Initialization/PlasmaInjector.cpp
@@ -548,13 +548,6 @@ bool PlasmaInjector::insideBounds (Real x, Real y, Real z) const noexcept
             z < zmax and z >= zmin);
 }
 
-#ifdef PULSAR
-bool PlasmaInjector::insidePulsarBounds (Real r, Real R_star, Real dR_star) const noexcept
-{
-    return (r <= R_star and r <= R_star - dR_star);
-}
-#endif
-
 bool PlasmaInjector::overlapsWith (const amrex::XDim3& lo,
                                    const amrex::XDim3& hi) const noexcept
 {
