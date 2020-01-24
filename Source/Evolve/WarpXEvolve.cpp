82b3e11806a51f1f1c570c00d98749790a8d766e
Donald E. Willcox <eugene.willcox@gmail.com>
2020-01-23 18:18:39 -0800

    Ran the style commands suggested by Travis CI to remove trailing whitespace.

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -233,7 +233,7 @@ WarpX::Evolve (int numsteps)
         else {
            amrex::Print() << " rho is computed \n";
         }
-        mypc->PulsarParticleRemoval(); 
+        mypc->PulsarParticleRemoval();
         mypc->PulsarParticleInjection();
 #endif
 
