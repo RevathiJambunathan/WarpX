b13630bc5c9c09f80bfbf618bb43d02d580db35a
RevathiJambunathan <revanathan@gmail.com>
2020-10-16 17:18:53 -0700

    remove unnecessary call to add external EB on EB Multifab. we call it directly in Fieldgather instead

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -434,10 +434,6 @@ WarpX::OneStep_nosub (Real cur_time)
     if (do_pml && pml_has_particles) CopyJPML();
     if (do_pml && do_pml_j_damping) DampJPML();
 
-//#ifdef PULSAR
-//    ApplyPulsarEBFieldsOnGrid();
-//#endif
-
     if( do_electrostatic == ElectrostaticSolverAlgo::None ) {
         // Electromagnetic solver:
         // Push E and B from {n} to {n+1}
