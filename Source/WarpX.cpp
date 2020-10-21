05f4e3e3ace7c9d87aaaba7921a87f90bf6718be
Revathi Jambunathan <revanathan@pop-os.localdomain>
2020-10-21 11:41:37 -0700

    make values real numbers and accout for SP/DP

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -87,16 +87,16 @@ std::string WarpX::str_Hy_excitation_grid_function;
 std::string WarpX::str_Hz_excitation_grid_function;
 #endif
 // Flag for type of excitation -1=none, 0=hard, 1=soft
-std::string WarpX::str_Ex_excitation_flag_function = "-1";
-std::string WarpX::str_Ey_excitation_flag_function = "-1";
-std::string WarpX::str_Ez_excitation_flag_function = "-1";
-std::string WarpX::str_Bx_excitation_flag_function = "-1";
-std::string WarpX::str_By_excitation_flag_function = "-1";
-std::string WarpX::str_Bz_excitation_flag_function = "-1";
+std::string WarpX::str_Ex_excitation_flag_function = "-1.";
+std::string WarpX::str_Ey_excitation_flag_function = "-1.";
+std::string WarpX::str_Ez_excitation_flag_function = "-1.";
+std::string WarpX::str_Bx_excitation_flag_function = "-1.";
+std::string WarpX::str_By_excitation_flag_function = "-1.";
+std::string WarpX::str_Bz_excitation_flag_function = "-1.";
 #ifdef WARPX_MAG_LLG
-std::string WarpX::str_Hx_excitation_flag_function = "-1";
-std::string WarpX::str_Hy_excitation_flag_function = "-1";
-std::string WarpX::str_Hz_excitation_flag_function = "-1";
+std::string WarpX::str_Hx_excitation_flag_function = "-1.";
+std::string WarpX::str_Hy_excitation_flag_function = "-1.";
+std::string WarpX::str_Hz_excitation_flag_function = "-1.";
 #endif
 
 #ifdef WARPX_MAG_LLG
