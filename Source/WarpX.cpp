086196a23f93e30f59016f3cb06cd370e368adeb
RevathiJambunathan <rjambunathan@lbl.gov>
2021-01-11 14:24:21 -0800

    deposit charge is modified and therefore not const

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -1085,9 +1085,9 @@ WarpX::AllocLevelMFs (int lev, const BoxArray& ba, const DistributionMapping& dm
     Efield_avg_fp[lev][2] = std::make_unique<MultiFab>(amrex::convert(ba,Ez_nodal_flag),dm,ncomps,ngE);
 
 #ifdef PULSAR
-    const bool deposit_charge =  do_dive_cleaning || plot_rho;
+    bool deposit_charge =  do_dive_cleaning || plot_rho;
 #else
-    const bool deposit_charge = do_dive_cleaning || (plot_rho && do_back_transformed_diagnostics);
+    bool deposit_charge = do_dive_cleaning || (plot_rho && do_back_transformed_diagnostics);
 #endif // pulsar endif
     if (WarpX::maxwell_solver_id == MaxwellSolverAlgo::PSATD) {
         deposit_charge = do_dive_cleaning || (plot_rho && do_back_transformed_diagnostics)
