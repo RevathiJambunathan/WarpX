1cf225e81f397eca10c57f06b40e9ece10fee6d4
RevathiJambunathan <revanathan@gmail.com>
2022-05-09 11:54:21 -0700

    compute ndens and mag before injecting particles
    for magnetization use cell-centered B, and compute sum of injection flag

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -122,6 +122,8 @@ WarpX::Evolve (int numsteps)
 
 #ifdef PULSAR
         m_pulsar->TotalParticles();
+        m_pulsar->ComputePlasmaNumberDensity();
+        m_pulsar->ComputePlasmaMagnetization();
         // inject particles for pulsar simulation
         if (Pulsar::m_singleParticleTest == 1) {
             if (Pulsar::m_continuous_injection == 0) {
@@ -350,12 +352,6 @@ WarpX::Evolve (int numsteps)
                 mypc->Redistribute();
             }
         }
-
-#ifdef PULSAR
-        m_pulsar->ComputePlasmaNumberDensity();
-        m_pulsar->ComputePlasmaMagnetization();
-#endif
-
 
         if (sort_intervals.contains(step+1)) {
             if (verbose) {
